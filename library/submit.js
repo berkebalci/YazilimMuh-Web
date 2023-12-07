ad2c4b81fa50f5bc3a89e32dcb18fe0e84bff580
ilayybaran <105604821+ilayybaran@users.noreply.github.com>
2023-12-07 18:41:33 +0300

    html css and js files of both pages

---
+++ library/submit.js
@@ -0,0 +1,25 @@
+const plus1=document.querySelector(".button1");
+const minus1=document.querySelector(".button2");
+const plus2=document.querySelector(".button3");
+const minus2=document.querySelector(".button4");
+const plus3=document.querySelector(".button5");
+const minus3=document.querySelector(".button6");
+
+plus1.addEventListener('click',()=>{
+
+})
+minus1.addEventListener('click',()=>{
+
+})
+plus2.addEventListener('click',()=>{
+
+})
+minus2.addEventListener('click',()=>{
+
+})
+plus3.addEventListener('click',()=>{
+
+})
+minus3.addEventListener('click',()=>{
+
+})
