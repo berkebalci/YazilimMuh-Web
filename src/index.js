7717f573bc6162865e59ce00f9f5ba6aa71c2311
Murus10 <mehmeturus73@gmail.com>
2023-12-10 13:57:45 +0300

    Frontend Kismi Basariyla Baglandi

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
+
+function validate(){
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var username_error = document.getElementById('username-error');
+  var password_error = document.getElementById('password-error');
+
+
+//validation for the inputs
+if(username ==''){
+  username_error.style.display = "block";
+  username.focus();
+  return false;
+}
+if(password ==''){
+  password_error.style.display = "block";
+  password.focus();
+  return false;
+}
+
+//redirecting to another page
+
+if(username=="kullanıcı"&&password=="sifre"){
+  
+  location.href="control.html";
+  return false;
+}
+else{
+  alert("Giriş başarısız")
+}
+
+}
+
+const show=()=>{
+  let password=document.getElementById("password");
+  let visibility=document.querySelector(".visibility");
+  if(password.type=== "password"){
+      password.type="text";
+      visibility.style.color="rgb(98,98,98)"
+  }
+  else{
+      password.type="password";
+      visibility.style.color="#F9F6F4"
+  }
+}
+
 /*
 import { initializeApp } from 'firebase/app'
 import {
@@ -13,6 +60,8 @@ import {
   signInWithEmailAndPassword, signOut,
   onAuthStateChanged
 } from 'firebase/auth'
+
+
 
 const firebaseConfig = {
 
@@ -37,30 +86,52 @@ initializeApp(firebaseConfig)
 
 // init services
 const db = getFirestore()
-const auth = getAuth()
+// const auth = getAuth()
 
-// collection ref
-const colRef = collection(db, 'kutuphaneler')
 
+
+
+// collection sesliBolumRef
+const colsesliBolumRef = collection(db, 'kutuphaneler')
+const akademikKisimsesliBolumRef = doc(db, 'kutuphaneler/talasKutuphanesi/bolumler/akademikKisim', 'kapasite');
+const sesliBolumsesliBolumRef = doc(db, 'kutuphaneler/talasKutuphanesi/bolumler/sesliBolum', 'kapasite');
+const sessizBolumsesliBolumRef = doc(db, 'kutuphaneler/talasKutuphanesi/bolumler/sessizBolum', 'kapasite');
+
+const button5 = document.querySelector("#akademikAlanArtirButton");
+const button6 = document.querySelector("#akademikAlanAzaltButton");
+
+const button1 = document.querySelector("#sesliBolumArtirButton");
+const button2 = document.querySelector("#sesliBolumAzaltButton");
+
+const button3 = document.querySelector("#sessizBolumArtirButton");
+const button4 = document.querySelector("#sessizBolumAzaltButton");
+
+
+
+*/
+/*
 // queries
-const q = query(colRef, where("author", "==", "patrick rothfuss"), orderBy('createdAt'))
+const q = query(colsesliBolumRef, where("author", "==", "patrick rothfuss"), orderBy('createdAt'))
+*/
 
+/*
 // realtime collection data
 const unsubCol = onSnapshot(q, (snapshot) => {
   let kutuphaneler = []
   snapshot.docs.forEach(doc => {
-    kutuphaneler.push({ ...doc.data(), id: doc.id })
+    kutuphaneler.push({ ...doc.data(), kapasite: doc.kapasite })
   })
   console.log(kutuphaneler)
 })
-
+*/
+/*
 // adding docs
 const addBookForm = document.querySelector('.add')
 addBookForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
-  addDoc(colRef, {
-    title: addBookForm.title.value,
+  addDoc(colsesliBolumRef, {
+    kapasite: addBookForm.kapasite.value,
     author: addBookForm.author.value,
     createdAt: serverTimestamp()
   })
@@ -68,42 +139,66 @@ addBookForm.addEventListener('submit', (e) => {
     addBookForm.reset()
   })
 })
-
+*/
+/*
 // deleting docs
 const deleteBookForm = document.querySelector('.delete')
 deleteBookForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
-  const docRef = doc(db, 'kutuphaneler', deleteBookForm.id.value)
+  const docsesliBolumRef = doc(db, 'kutuphaneler', deleteBookForm.id.value)
 
-  deleteDoc(docRef)
+  deleteDoc(docsesliBolumRef)
     .then(() => {
       deleteBookForm.reset()
     })
 })
+*/
 
+/*
 // fetching a single document (& realtime)
-const docRef = doc(db, 'kutuphaneler', 'gGu4P9x0ZHK9SspA1d9j')
-
-const unsubDoc = onSnapshot(docRef, (doc) => {
+const docsesliBolumRef = doc(db, 'kutuphaneler', 'gGu4P9x0ZHK9SspA1d9j')
+*/
+/*
+const unsubDoc = onSnapshot(docsesliBolumRef, (doc) => {
   console.log(doc.data(), doc.id)
 })
-
+*/
+/*
 // updating a document
 const updateForm = document.querySelector('.update')
 updateForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
-  let docRef = doc(db, 'kutuphaneler', updateForm.id.value)
+  let sesliBolumsesliBolumRef = doc(db, 'kutuphaneler', updateForm.kapasite.value)
 
-  updateDoc(docRef, {
-    title: 'updated title'
+  updateDoc(sesliBolumsesliBolumRef, {
+    kapasite: 'updated kapasite'
   })
   .then(() => {
     updateForm.reset()
   })
 })
 
+
+function arttir(sesliBolumRef) {
+  sesliBolumRef.once('value', (snapshot) => {
+    const dolulukOrani = snapshot.val() || 0;
+    sesliBolumRef.set(dolulukOrani + 1);
+  });
+}
+
+// Azaltma işlevi
+function azalt(sesliBolumRef) {
+  sesliBolumRef.once('value', (snapshot) => {
+    const dolulukOrani = snapshot.val() || 0;
+    if (dolulukOrani > 0) {
+      sesliBolumRef.set(dolulukOrani - 1);
+    }
+  });
+}
+*/
+/*
 // signing users up
 const signupForm = document.querySelector('.signup')
 signupForm.addEventListener('submit', (e) => {
@@ -150,7 +245,8 @@ loginForm.addEventListener('submit', (e) => {
       console.log(err.message)
     })
 })
-
+*/
+/*
 // subscribing to auth changes
 const unsubAuth = onAuthStateChanged(auth, (user) => {
   console.log('user status changed:', user)
@@ -164,34 +260,18 @@ unsubButton.addEventListener('click', () => {
   unsubDoc()
   unsubAuth()
 })
+*/
 
 
-// Arttırma işlevi
-function arttir(ref) {
-  ref.once('value', (snapshot) => {
-    const dolulukOrani = snapshot.val() || 0;
-    ref.set(dolulukOrani + 1);
-  });
-}
 
-// Azaltma işlevi
-function azalt(ref) {
-  ref.once('value', (snapshot) => {
-    const dolulukOrani = snapshot.val() || 0;
-    if (dolulukOrani > 0) {
-      ref.set(dolulukOrani - 1);
-    }
-  });
-}
-
-// ... (Önceki kodlar)
-
-// Referanslar
-const akademikKisimRef = doc(db, 'kutuphaneler/talasKutuphanesi/bolumler/akademikKisim', 'kapasite');
-const sesliBolumRef = doc(db, 'kutuphaneler/talasKutuphanesi/bolumler/sesliBolum', 'kapasite');
-const sessizBolumRef = doc(db, 'kutuphaneler/talasKutuphanesi/bolumler/sessizBolum', 'kapasite');
 
 /*
+// sesliBolumReferanslar
+const akademikKisimsesliBolumRef = doc(db, 'kutuphaneler/talasKutuphanesi/bolumler/akademikKisim', 'kapasite');
+const sesliBolumsesliBolumRef = doc(db, 'kutuphaneler/talasKutuphanesi/bolumler/sesliBolum', 'kapasite');
+const sessizBolumsesliBolumRef = doc(db, 'kutuphaneler/talasKutuphanesi/bolumler/sessizBolum', 'kapasite');
+
+
 // İlgili butonlar ve etkinlik dinleyicileri
 const button5 = document.querySelector("#akademikAlanArtirButton");
 const button6 = document.querySelector("#akademikAlanAzaltButton");
@@ -201,83 +281,18 @@ const button2 = document.querySelector("#sesliBolumAzaltButton");
 
 const button3 = document.querySelector("#sessizBolumArtirButton");
 const button4 = document.querySelector("#sessizBolumAzaltButton");
-*/
-
-
-
-/*
-const button1 = document.querySelector(".button1");
-const button2 = document.querySelector(".button2");
-const button3 = document.querySelector(".button3");
-const button4 = document.querySelector(".button4");
-const button5 = document.querySelector(".button5");
-const button6 = document.querySelector(".button6");
-button5.addEventListener("click", () => arttir(akademikAlanRef.value));
-button6.addEventListener("click", () => azalt(akademikAlanRef.value));
-
-button1.addEventListener("click", () => arttir(sesliBolumRef.value));
-button2.addEventListener("click", () => azalt(sesliBolumRef.value));
-
-button3.addEventListener("click", () => arttir(sessizBolumRef.value));
-button4.addEventListener("click", () => azalt(sessizBolumRef.value));
-
-// ... (Devam eden kodlar)
-
-*/
-
-
-
-
-
-
-/*
-
-
-function validate(){
-
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var username_error = document.getElementById('username-error');
-    var password_error = document.getElementById('password-error');
-
-
-  //validation for the inputs
-if(username ==''){
-    username_error.style.display = "block";
-    username.focus();
-    return false;
-}
-if(password ==''){
-    password_error.style.display = "block";
-    password.focus();
-    return false;
-}
-
-  //redirecting to another page
-
-if(username=="kullanıcı"&&password=="sifre"){
-    
-    location.href="control.html";
-    return false;
-}
-else{
-    alert("Giriş başarısız")
-}
-
-}
-
-const show=()=>{
-    let password=document.getElementById("password");
-    let visibility=document.querySelector(".visibility");
-    if(password.type=== "password"){
-        password.type="text";
-        visibility.style.color="rgb(98,98,98)"
-    }
-    else{
-        password.type="password";
-        visibility.style.color="#F9F6F4"
-    }
-}
 
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
