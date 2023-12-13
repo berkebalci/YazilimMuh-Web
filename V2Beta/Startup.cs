a2396eb1bacef8c66ba3f6350d64b3df75827488
Furkan Çelik <furkanomer175337@gmail.com>
2023-12-14 01:07:17 +0300

    Cors Policy sorunu düzeltildi. Projenin neredeyse son haline gelindi.

--- V2Beta/Startup.cs
+++ V2Beta/Startup.cs
@@ -20,10 +20,10 @@ public class Startup
         // CORS politikasını ekleyin
         services.AddCors(options =>
         {
-            options.AddPolicy(name: "MyPolicy",
+            options.AddPolicy("AllowSpecificOrigin",
                               builder =>
                               {
-                                  builder.WithOrigins("http://example.com")
+                                  builder.WithOrigins("http://127.0.0.1:5500")
                                          .AllowAnyMethod()
                                          .AllowAnyHeader();
                               });
@@ -38,7 +38,7 @@ public class Startup
         }
 
         // CORS'u etkinleştirin
-        app.UseCors("MyPolicy");
+        app.UseCors("AllowSpecificOrigin");
 
         app.UseRouting();
 
@@ -93,85 +93,4 @@ public class Startup
         });
     }
 }
-/*using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using FirebaseAdmin;
-using Google.Cloud.Firestore;
-using Google.Apis.Auth.OAuth2;
-public class Startup
-{
 
-    
-
-    public void ConfigureServices(IServiceCollection services)
-    {   services.AddControllers();
-        FirebaseApp.Create(new AppOptions
-    {
-        Credential = GoogleCredential.FromFile("talaskutuphaneleri-firebase-adminsdk-b6jxl-d2aa91a8e2.json"),
-    });
-    }
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-{
-    if (env.IsDevelopment())
-    {
-        app.UseDeveloperExceptionPage();
-    }
-
-    app.UseRouting();
-
-    app.UseEndpoints(endpoints =>
-    {
-        endpoints.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}"
-        );
-        
-        string projectId = "talaskutuphaneleri";
-
-        // Firestore bağlantısı oluşturma
-        var firestore = FirestoreDb.Create(projectId);
-        Console.WriteLine("Firestore bağlantısı başarıyla oluşturuldu.");
-
-        // "kutuphaneler" koleksiyonunu referans alma
-        CollectionReference kutuphanelerCollection = firestore.Collection("kutuphaneler");
-
-        QuerySnapshot kutuphaneSnapshot = kutuphanelerCollection.GetSnapshotAsync().Result;
-
-        // Her belge için Id ve değerleri yazdırma
-        foreach (DocumentSnapshot documentSnapshot in kutuphaneSnapshot.Documents)
-        {
-            string kutuphaneId = documentSnapshot.Id;
-            Console.WriteLine($"Kütüphane Id: {kutuphaneId}");
-
-            // Belge içeriğini almak ve Kutuphane sınıfına map etmek
-            Kutuphane kutuphane = documentSnapshot.ConvertTo<Kutuphane>();
-
-            // Kütüphane adı ve kapasitesini yazdırmak
-            Console.WriteLine($"Kütüphane Adı: {kutuphane.KutuphaneAdi}");
-
-            CollectionReference bolumlerCollection = documentSnapshot.Reference.Collection("bolumler");
-            QuerySnapshot bolumlerSnapshot = bolumlerCollection.GetSnapshotAsync().Result;
-
-            // Her belge için Id ve değerleri yazdır
-            foreach (DocumentSnapshot bolumSnapshot in bolumlerSnapshot.Documents)
-            {
-                string bolumId = bolumSnapshot.Id;
-                Console.WriteLine($"Bolum Id: {bolumId}");
-
-                Dictionary<string, object> bolumData = bolumSnapshot.ToDictionary();
-                foreach (var field in bolumData)
-                {
-                    Console.WriteLine($"{field.Key}: {field.Value}");
-                }
-            }
-
-            Console.WriteLine();
-        }
-    });
-}
-
-
-}*/
